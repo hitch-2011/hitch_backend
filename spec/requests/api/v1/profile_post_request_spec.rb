6e371da7b1be0e4ff3c19f6262896c0d3afc6f3a
Jake Volpe <volpe.jacob@gmail.com>
2021-05-17 16:17:44 -0600

    Add spec_helper method, prof create request test
    Co-authored-by: Dominic Padula <70593322+domo2192@users.noreply.github.com>
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

---
+++ spec/requests/api/v1/profile_post_request_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Users Create', type: :request do 
+  describe 'happy path' do 
+    it 'should return a 201 when a user is created with valid params' do 
+      valid_params = {
+                      email: 'example@email.com',
+                      password: '123',
+                      password_confirmation: '123',
+                      about_me: 'Hi my name is example, I drive a whole bunch and want to carpool'
+                     }
+      
+      post api_v1_users_path, params: valid_params
+      expect(response).to have_http_status(201)
+    end
+  end
+  describe 'sad path' do 
+    it 'returns an error message if no email is present' do 
+      invalid_params = {
+        password: '123',
+        password_confirmation: '123',
+        about_me: 'Hi my name is example, I drive a whole bunch and want to carpool'
+       }
+
+       post api_v1_users_path, params: invalid_params
+       json = JSON.parse(response.body, symbolize_names: true)
+       check_hash_structure(json, :error, String)
+       expect(json[:error]).to eq("invalid parameters")
+    end
+    it 'returns an error message if passwords dont match' do 
+      invalid_params = {
+        email: 'example@email.com',
+        password: '123',
+        password_confirmation: '12345',
+        about_me: 'Hi my name is example, I drive a whole bunch and want to carpool'
+       }
+
+       post api_v1_users_path, params: invalid_params
+       json = JSON.parse(response.body, symbolize_names: true)
+       check_hash_structure(json, :error, String)
+       expect(json[:error]).to eq("invalid parameters")
+    end
+  end
+end
