cf66b5ccfa2798ed378d8ce1b0d242cb6e4e090d
Jake Volpe <volpe.jacob@gmail.com>
2021-05-17 16:47:21 -0600

    Add more sad path for profile post request
    Co-authored-by: Dominic Padula <70593322+domo2192@users.noreply.github.com>
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,6 @@ class Api::V1::UsersController < ApplicationController
 
   private 
   def user_params
-    params.permit(:email, :password, :about_me)
+    params.permit(:email, :password, :password_confirmation, :about_me)
   end
 end
