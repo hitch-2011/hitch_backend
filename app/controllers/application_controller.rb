ac10094f02495eb0b4e4c85d057d604d2ac5836e
Dominic Padula <domopadula2192@gmail.com>
2021-05-18 08:13:46 -0600

    create show action
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+
+  def render_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def render_invalid_record(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def render_error(error, status = :bad_request)
+    render json: { message: "your request cannot be completed", error: error}, status: status
+  end
+
+
+  def validate_id
+    id = :"#{params.keys.grep(/id/)[0]}"
+    error = "String not accepted as id"
+    render_error(error) if params[id].to_i == 0
+  end
 end
