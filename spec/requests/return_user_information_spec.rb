321af3c83b64b286cdcd6e0adaf909922a7745fd
Dominic Padula <domopadula2192@gmail.com>
2021-05-18 16:43:43 -0600

    correct tests for return user
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- spec/requests/return_user_information_spec.rb
+++ spec/requests/return_user_information_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "Users API Endpoints" do
   describe "User Details" do
     it 'should return user details details based on id' do
-    user1 = User.create!(email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
-    user2 = User.create!(email: "jake@gmail.com", password: "password", about_me: "I like driving.")
-    user3 = User.create!(email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
+    user1 = User.create(fullname: "fullname", email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
+    user2 = User.create!(fullname: "fullname", email: "jake@gmail.com", password: "password", about_me: "I like driving.")
+    user3 = User.create!(fullname: "fullname", email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
     friend = Friend.create!(user_id: user1.id, friend_id: user2.id)
     friend = Friend.create!(user_id: user1.id, friend_id: user3.id)
     @ride = Ride.create!(origin: "origin", destination: "destination", departure_time: "9:00am", user_id: user1.id )
@@ -19,7 +19,9 @@ RSpec.describe "Users API Endpoints" do
       check_hash_structure(profile_details[:data], :type, String)
       expect(profile_details[:data][:type]).to eq("profile")
       check_hash_structure(profile_details[:data][:attributes], :email, String)
+      check_hash_structure(profile_details[:data][:attributes], :fullname, String)
       expect(profile_details[:data][:attributes][:email]).to eq("#{user1.email}")
+      expect(profile_details[:data][:attributes][:fullname]).to eq("#{user1.fullname}")
       check_hash_structure(profile_details[:data][:attributes], :email, String)
       check_hash_structure(profile_details[:data][:attributes], :about_me, String)
       expect(profile_details[:data][:attributes][:about_me]).to eq("#{user1.about_me}")
@@ -40,6 +42,7 @@ RSpec.describe "Users API Endpoints" do
     check_hash_structure(profile_details[:data][:attributes], :friends, Hash)
       profile_details[:data][:attributes][:friends][:data].each do |ref|
         check_hash_structure(ref[:attributes], :email, String)
+        check_hash_structure(ref[:attributes], :fullname, String)
         check_hash_structure(ref[:attributes], :about_me, String)
         check_hash_structure(ref, :type, String)
         check_hash_structure(ref, :id, String)
@@ -47,9 +50,9 @@ RSpec.describe "Users API Endpoints" do
     end
 
     it 'still works with less less than 1 ride or friend' do
-      user1 = User.create!(email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
-      user2 = User.create!(email: "jake@gmail.com", password: "password", about_me: "I like driving.")
-      user3 = User.create!(email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
+      user1 = User.create!(fullname: "fullname", email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
+      user2 = User.create!(fullname: "fullname", email: "jake@gmail.com", password: "password", about_me: "I like driving.")
+      user3 = User.create!(fullname: "fullname", email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
       get "/api/v1/users/#{user1.id}"
       expect(response).to be_successful
       profile_details = JSON.parse(response.body, symbolize_names:true)
@@ -60,9 +63,9 @@ RSpec.describe "Users API Endpoints" do
     end
 
     it 'allows for multiple rides' do
-      user1 = User.create!(email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
-      user2 = User.create!(email: "jake@gmail.com", password: "password", about_me: "I like driving.")
-      user3 = User.create!(email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
+      user1 = User.create!(fullname: "fullname", email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
+      user2 = User.create!(fullname: "fullname", email: "jake@gmail.com", password: "password", about_me: "I like driving.")
+      user3 = User.create!(fullname: "fullname", email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
       friend = Friend.create!(user_id: user1.id, friend_id: user2.id)
       friend = Friend.create!(user_id: user1.id, friend_id: user3.id)
       trip = Ride.create!(origin: "origin", destination: "destination", departure_time: "9:00am", user_id: user1.id )
@@ -76,10 +79,10 @@ RSpec.describe "Users API Endpoints" do
 
 
     it 'does not grab other rides' do
-      user1 = User.create!(email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
-      user2 = User.create!(email: "jake@gmail.com", password: "password", about_me: "I like driving.")
-      user3 = User.create!(email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
-      user4= User.create!(email: "differnt@gmail.com", password: "password", about_me: "I like driving.")
+      user1 = User.create!(fullname: "fullname", email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
+      user2 = User.create!(fullname: "fullname", email: "jake@gmail.com", password: "password", about_me: "I like driving.")
+      user3 = User.create!(fullname: "fullname", email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
+      user4= User.create!(fullname: "fullname", email: "differnt@gmail.com", password: "password", about_me: "I like driving.")
       friend = Friend.create!(user_id: user1.id, friend_id: user2.id)
       friend = Friend.create!(user_id: user1.id, friend_id: user3.id)
       trip = Ride.create!(origin: "origin", destination: "destination", departure_time: "9:00am", user_id: user1.id )
@@ -94,11 +97,11 @@ RSpec.describe "Users API Endpoints" do
     end
 
     it 'does not grab other rides' do
-      user1 = User.create!(email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
-      user2 = User.create!(email: "jake@gmail.com", password: "password", about_me: "I like driving.")
-      user3 = User.create!(email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
-      user4= User.create!(email: "differnt@gmail.com", password: "password", about_me: "I like driving.")
-      user5 = User.create!(email: "new@gmail.com", password: "password", about_me: "I like driving.")
+      user1 = User.create!(fullname: "fullname", email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
+      user2 = User.create!(fullname: "fullname", email: "jake@gmail.com", password: "password", about_me: "I like driving.")
+      user3 = User.create!(fullname: "fullname", email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
+      user4= User.create!(fullname: "fullname", email: "differnt@gmail.com", password: "password", about_me: "I like driving.")
+      user5 = User.create!(fullname: "fullname", email: "new@gmail.com", password: "password", about_me: "I like driving.")
       friend = Friend.create!(user_id: user1.id, friend_id: user2.id)
       friend = Friend.create!(user_id: user1.id, friend_id: user3.id)
       friend = Friend.create!(user_id: user1.id, friend_id: user5.id)
@@ -120,9 +123,9 @@ RSpec.describe "Users API Endpoints" do
 
   describe 'sad path' do
     it 'breaks without properly formatted id' do
-      user1 = User.create!(email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
-      user2 = User.create!(email: "jake@gmail.com", password: "password", about_me: "I like driving.")
-      user3 = User.create!(email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
+      user1 = User.create!(fullname: "fullname", email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
+      user2 = User.create!(fullname: "fullname", email: "jake@gmail.com", password: "password", about_me: "I like driving.")
+      user3 = User.create!(fullname: "fullname", email: "cydnee@gmail.com", password: "password", about_me: "I like driving.")
       friend = Friend.create!(user_id: user1.id, friend_id: user2.id)
       friend = Friend.create!(user_id: user1.id, friend_id: user3.id)
       get "/api/v1/users/one"
@@ -132,14 +135,14 @@ RSpec.describe "Users API Endpoints" do
     end
 
     it 'breaks if the id does not exist' do
-      user1 = User.create!(email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
+      user1 = User.create!(fullname: "fullname", email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
       get "/api/v1/users/1000000000000"
       profile_details = JSON.parse(response.body, symbolize_names:true)
       expect(profile_details[:error]).to eq("Couldn't find User with 'id'=1000000000000")
     end
 
     it 'breaks when it is empty string' do
-      user1 = User.create!(email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
+      user1 = User.create!(fullname: "fullname", email: "dominic@gmail.com", password: "password", about_me: "I like driving.")
       get "/api/v1/users/''"
       profile_details = JSON.parse(response.body, symbolize_names:true)
       expect(profile_details[:message]).to eq("your request cannot be completed")
