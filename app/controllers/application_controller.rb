c630fbfc7b2cad2dbd0ff79cedcfd721b508cd4c
Dominic Padula <domopadula2192@gmail.com>
2021-05-18 14:58:51 -0600

    add before action for show action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
 
   def render_not_found(exception)
-    render json: { error: exception.message }, status: :not_found
-  end
-
-  def render_invalid_record(exception)
     render json: { error: exception.message }, status: :not_found
   end
 
