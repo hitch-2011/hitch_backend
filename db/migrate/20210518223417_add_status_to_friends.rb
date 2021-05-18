5605633c87848ba57a8c4e9af7e36e553177c56c
Dominic Padula <domopadula2192@gmail.com>
2021-05-18 16:42:05 -0600

    add enum status with default
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- db/migrate/20210518223417_add_status_to_friends.rb
+++ db/migrate/20210518223417_add_status_to_friends.rb
@@ -1,5 +1,5 @@
 class AddStatusToFriends < ActiveRecord::Migration[5.2]
   def change
-    add_column :friends, :status, :integer
+    add_column :friends, :status, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_220637) do
+ActiveRecord::Schema.define(version: 2021_05_18_223417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_18_220637) do
     t.bigint "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["friend_id"], name: "index_friends_on_friend_id"
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
